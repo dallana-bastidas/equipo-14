4eeec7b0d8fe3686f5a94471d61648ee79732189
joelocano <joelangelocano@gmail.com>
2024-10-07 14:39:13 -0500

    removed fronend

--- Frontend/stockit/src/app/app.config.server.ts
+++
@@ -1,6 +0,0 @@
-import { ApplicationConfig } from "@angular/core";
-
-export const config: ApplicationConfig = {
-    production: false,
-    providers: []
-};
--- Frontend/stockit/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { AñadirProductoComponent } from './components/add-product/add-product.component';
-
-
-export const routes: Routes = [
-    {path: 'add-product', component: AñadirProductoComponent}
-];
