7d43d3c0f66a7d610372ba5a2e7dcc8e08ffd3c3
joelocano <joelangelocano@gmail.com>
2024-10-07 14:37:23 -0500

    feat: added daysiui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,5 +4,35 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#2563EB",
+          "primary-content": "#FFFFFF",
+          secondary: "#FFC358",
+          "secondary-content": "#160E03",
+          accent: "#22D3EE",
+          "accent-content": "#001014",
+          neutral: "#011132",
+          "neutral-content": "#c4c9ce",
+          "base-100": "#FFFFFF",
+          "base-200": "#F2F2F2",
+          "base-300": "#BEBEBE",
+          "base-content": "#011132",
+          info: "#00a6ff",
+          "info-content": "#000a16",
+          success: "#34d399",
+          "success-content": "#011008",
+          warning: "#facc15",
+          "warning-content": "#150f00",
+          error: "#E11D48",
+          "error-content": "#FFD8D9",
+        },
+      },
+    ],
+  },
 };
