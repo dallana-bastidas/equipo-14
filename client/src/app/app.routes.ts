2eab219e734000446bb9ce55db5f9af93892d56f
Dallana Bastidas <cindydallana2822@gmail.com>
2024-10-08 17:50:15 -0500

    tabla inventario

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import { InventarioComponent } from './inventario/inventario.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [{ path: 'navbar', component: NavbarComponent }];
+export const routes: Routes = [
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'inventario', component: InventarioComponent },
+];
