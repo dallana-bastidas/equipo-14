6c3598d4b957b67ba37ef2fe9c027ddbea95b0e7
Dallana Bastidas <cindydallana2822@gmail.com>
2024-10-08 19:55:31 -0500

    navbar y inventario

--- client/src/app/inventario/inventario.component.ts
+++ client/src/app/inventario/inventario.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-inventario',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './inventario.component.html',
   styleUrl: './inventario.component.css'
 })
