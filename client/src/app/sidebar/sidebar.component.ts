ac45e80dcacc97166cd0e572518f7eaf29d2ddb9
Juan <juanndiiego2@gmail.com>
2024-10-11 02:28:30 -0500

    Sidebar diseño y frontend/ falta backend

--- client/src/app/sidebar/sidebar.component.ts
+++ client/src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrls: ['./sidebar.component.css'],
+  imports: [CommonModule],
 })
 export class SidebarComponent {
+  inventoryOpen = false;
+  providersOpen = false;
 
+
+  constructor(private router: Router) {}
+
+
+  // Método para alternar el estado del submenú de inventario
+  toggleInventory() {
+    this.inventoryOpen = !this.inventoryOpen;
+  }
+
+
+  // Método para alternar el estado del submenú de proveedores
+  toggleProviders() {
+    this.providersOpen = !this.providersOpen;
+  }
 }
