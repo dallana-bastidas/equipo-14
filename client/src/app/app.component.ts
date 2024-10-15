e2337dacc1c5ee4e003b96a62abed111d6776e39
Juan <juanndiiego2@gmail.com>
2024-10-14 20:37:05 -0500

    Se soluciona conflicto de componentes duplicados

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FiltrosComponent } from './filtros/filtros.component';
-import { InventarioComponent } from './inventario/inventario.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InventarioComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
