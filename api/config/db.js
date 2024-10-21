1c33b8baf3fcfe173f8bd39396065bb7a7557130
Juan <juanndiiego2@gmail.com>
2024-10-21 13:32:52 -0500

    deploy

--- api/config/db.js
+++ api/config/db.js
@@ -1,14 +1,29 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// const conectarDb = async () => {
+//     try {
+//         await mongoose.connect(process.env.DB_MONGO)
+//         console.log('DB esta conectada');
+//     } catch (error) {
+//         console.log(error);
+//         process.exit(1)
+//     }
+// }
+
+// module.exports = conectarDb
+
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const conectarDb = async () => {
     try {
-        await mongoose.connect(process.env.DB_MONGO)
-        console.log('DB esta conectada');
+        await mongoose.connect(process.env.DB_MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('DB está conectada');
     } catch (error) {
         console.log(error);
-        process.exit(1)
+        process.exit(1);
     }
 }
 
-module.exports = conectarDb
+module.exports = conectarDb;
--- api/index.js
+++ api/index.js
@@ -2,18 +2,38 @@
 
 //creacion del sevidor
 
-const express = require('express');
-const conectarDb = require('./config/db')
-const cors = require("cors")
+// const express = require('express');
+// const conectarDb = require('./config/db')
+// const cors = require("cors")
 
-const app = express()
-conectarDb()
-app.use(cors())
+// const app = express()
+// conectarDb()
+// app.use(cors())
+// app.use(express.json());
+
+// app.use('/api', require('./routes/rutas_aplicacion'))
+// app.use('/uploads', express.static('uploads'));
+
+// app.listen(5200, () => {
+// 	console.log('el servidor esta arriba');
+// })
+
+
+const express = require('express');
+const conectarDb = require('./config/db');
+const cors = require("cors");
+
+const app = express();
+
+// Conectar a la base de datos
+conectarDb();
+
+app.use(cors());
 app.use(express.json());
 
-app.use('/api', require('./routes/rutas_aplicacion'))
+app.use('/api', require('./routes/rutas_aplicacion'));
 app.use('/uploads', express.static('uploads'));
 
 app.listen(5200, () => {
-	console.log('el servidor esta arriba');
-})
+    console.log('El servidor está arriba en el puerto 5200');
+});
